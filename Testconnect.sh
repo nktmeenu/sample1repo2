bc02230b74569970c5b5e179a4e24e7f3e483c97
Nirmal Chandak <Nirmal.Chandak@rci.rogers.ca>
2018-09-23 14:55:11 -0400

    Modified and added env file

--- Testconnect.sh
+++ Testconnect.sh
@@ -1,37 +1,28 @@
 #!/bin/bash
 
-export HOME=/opt/Informatica9/infa9
+#Sourcing the env variable for the user executing the script
+source ./environmental_variables.env
 
-. $HOME/workspace/Informatica_Migration/scripts/Test/Infa_Environmental_Variables.env
+#Defining VARIABLES to be used in the script
+InfaMigPath=$(pwd)
+echo "$InfaMigPath"
 
-InfaMigPath=$HOME/workspace/Informatica_Migration
-LogFileDir=$InfaMigPath/logs
-date=`date +'%Y-%m-%d %H%M%S'`
-LogFileName=Infa_DG_NC_$date.log
-#export USERNAME=$1
-#export PASSWORD=$2
-#export SRC_REP=$3
-#export TGT_REP=$4
-#export NAME=$5
-#export ACTION=$6
 
-##### Nullifying the log file
-cat /dev/null>$LogFileDir/$LogFileName
+#Setting the parameters passed in the script as env variables for the session
 
-##### Connecting to the Source repository
-$PMSERVERDIR/pmrep connect -r $SRC_REP -d $DOMAIN -n $USERNAME -x $PASSWORD -s $USERSECURITYDOMAIN 2>>$LogFileDir/$LogFileName 1>>$LogFileDir/$LogFileName
+##### Connecting to the Source repository, $DOMAIN defined in the env file
+echo "Connecting to the Repository "$SRC_REP
+pmrep connect -r $SRC_REP -d $DOMAIN -n $USERNAME -x $PASSWORD -s $USERSECURITYDOMAIN
 RETURN_CODE=$?
-echo "RETURN_CODE: "$RETURN_CODE  >>$LogFileDir/$LogFileName
+echo "RETURN_CODE: "$RETURN_CODE
 
-        if [ $RETURN_CODE == 0 ]
-        then
-        echo "Connected to the Repository "$SRC_REP
-        echo
-        echo "Connected to the Repository "$SRC_REP >>$LogFileDir/$LogFileName
-        else
-        echo "Failed to Connect to the Repository "$SRC_REP
-        echo "Failed to Connect to the Repository "$SRC_REP >>$LogFileDir/$LogFileName
-        echo "Check the log file "$LogFileDir/$LogFileName
-        echo
-        exit 1
-        fi
+	if [ $RETURN_CODE == 0 ]
+	then
+	echo "Connected to the Repository "$SRC_REP
+	echo
+	echo "Connected to the Repository "$SRC_REP
+	else
+	echo "Failed to Connect to the Repository "$SRC_REP
+	echo
+	exit 1
+	fi
