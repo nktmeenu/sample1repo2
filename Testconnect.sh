6a55b915f055505f44341e11f6c2c3301b6a57de
Nirmal Chandak <Nirmal.Chandak@rci.rogers.ca>
2018-09-23 15:21:17 -0400

    modified

--- Testconnect.sh
+++ Testconnect.sh
@@ -4,33 +4,29 @@ source ./environmental_variables.env
 
 InfaMigPath=$HOME/workspace/Informatica_Migration
 echo "$InfaMigPath"
-LogFileDir=$InfaMigPath/logs
-date=`date +'%Y-%m-%d %H%M%S'`
-LogFileName=Infa_DG_NC_$date.log
-export USERNAME=$1
-export PASSWORD=$2
-export SRC_REP=$3
-export TGT_REP=$4
-export NAME=$5
-export ACTION=$6
 
-##### Nullifying the log file
-cat /dev/null>$LogFileDir/$LogFileName
+echo "Source repository is " $SRC_REP
+echo "Username is " $USERNAME
+echo "password is " $PASSWORD
+echo "domain is " $DOMAIN
+echo "pmserverdir is " $PMSERVERDIR
+echo "usersecurity domain is " $USERSECURITYDOMAIN
+echo "Migration path is " $InfaMigPath
 
 ##### Connecting to the Source repository
-$PMSERVERDIR/pmrep connect -r $SRC_REP -d $DOMAIN -n $USERNAME -x $PASSWORD -s $USERSECURITYDOMAIN 2>>$LogFileDir/$LogFileName 1>>$LogFileDir/$LogFileName
+$PMSERVERDIR/pmrep connect -r $SRC_REP -d $DOMAIN -n $USERNAME -x $PASSWORD -s $USERSECURITYDOMAIN
 RETURN_CODE=$?
-echo "RETURN_CODE: "$RETURN_CODE  >>$LogFileDir/$LogFileName
+echo "RETURN_CODE: "$RETURN_CODE 
 
         if [ $RETURN_CODE == 0 ]
         then
         echo "Connected to the Repository "$SRC_REP
         echo
-        echo "Connected to the Repository "$SRC_REP >>$LogFileDir/$LogFileName
+        echo "Connected to the Repository "$SRC_REP 
         else
         echo "Failed to Connect to the Repository "$SRC_REP
-        echo "Failed to Connect to the Repository "$SRC_REP >>$LogFileDir/$LogFileName
-        echo "Check the log file "$LogFileDir/$LogFileName
+        echo "Failed to Connect to the Repository "$SRC_REP 
+        #echo "Check the log file "$LogFileDir/$LogFileName
         echo
         exit 1
         fi
